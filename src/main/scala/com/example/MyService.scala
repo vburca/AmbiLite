2458f0061a160c963e1b72de5db089699c77d366
Vlad Burca <vladburca2005@gmail.com>
2016-02-28 05:25:57 -0500

    Initial commit to AmbiLite

---
+++ src/main/scala/com/example/MyService.scala
@@ -0,0 +1,43 @@
+package com.example
+
+import akka.actor.Actor
+import spray.routing._
+import spray.http._
+import MediaTypes._
+
+import scalalirc._
+
+// we don't implement our route structure directly in the service actor because
+// we want to be able to test it independently, without having to spin up an actor
+class MyServiceActor extends Actor with MyService {
+
+  // the HttpService trait defines only one abstract member, which
+  // connects the services environment to the enclosing actor or test
+  def actorRefFactory = context
+
+  // this actor only runs our route, but you could add
+  // other things here, like request stream processing
+  // or timeout handling
+  def receive = runRoute(myRoute)
+}
+
+
+// this trait defines our service behavior independently from the service actor
+trait MyService extends HttpService {
+  val lirc = new ScalaLirc()
+
+  val myRoute = {
+    path("On") {
+      get {
+        lirc.sendOnce("AmbiLite", "BTN_START")
+        complete("Turning lights On!")
+      }
+    } ~
+    path("Off") {
+      get {
+        lirc.sendOnce("AmbiLite", "KEY_STOP")
+        complete("Turning lights Off!")
+      }
+    }
+  }
+}
